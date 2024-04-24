7c41d5cb1ac5479ed4b1049b81fd33ab8274adab
Florian Vazeille <florian.vazeille@pm.me>
2024-04-24 17:10:33 +0200

    Update fiber_2D.py

--- Model Creation/fiber_2D.py
+++ Model Creation/fiber_2D.py
@@ -51,8 +51,8 @@ def create_2d_rve_with_fibers(ep, density, num_fibers, fiber_length, ref_angle, 
     inner_box = gmsh.model.occ.addRectangle(0, 0, 0, width, height)
     gmsh.model.occ.synchronize()
     print(gmsh.model.getEntities(2))
-    outer_surf = gmsh.model.occ.addPlaneSurface([outer_box])
-    inner_surf = gmsh.model.occ.addPlaneSurface([inner_box])
+    #outer_surf = gmsh.model.occ.addPlaneSurface([outer_box])
+    #inner_surf = gmsh.model.occ.addPlaneSurface([inner_box])
 
 
 
@@ -62,7 +62,7 @@ def create_2d_rve_with_fibers(ep, density, num_fibers, fiber_length, ref_angle, 
     transfiniteAuto = True
     gmsh.model.mesh.setTransfiniteAutomatic()
     #if transfinite:
-    ov, ovv = gmsh.model.occ.fragment([(2, outer_surf)], [(2, inner_surf)])
+    ov, ovv = gmsh.model.occ.fragment([(2, outer_box)], [(2, inner_box)])
 
     gmsh.model.occ.synchronize()
 
@@ -97,23 +97,48 @@ def create_2d_rve_with_fibers(ep, density, num_fibers, fiber_length, ref_angle, 
         fiber_line = gmsh.model.occ.addLine(p1,p2)
         #gmsh.model.mesh.setSize([(1,fiber_line)], 0.05)
         #gmsh.model.mesh.embed(1, [fiber_line], 2, 2)
-        try:
-            ov, ovv = gmsh.model.occ.fragment([(2,inner_surf)], [(1, fiber_line)])
-        except:
-            print("pas possible")
         gmsh.model.occ.synchronize()
         fibers.append({"id": fiber_line, "position": [center_x, center_y], "angle": angle, "length": fiber_length})
         ax.plot([x1_adj, x2_adj], [y1_adj, y2_adj], marker='o')
 
+        if len(fibers) > 1:
+            lines = gmsh.model.getEntities(1)
+            sousseg = 1
+            tool = [(1,fiber_line)]
+            for i in range(len(lines)-9):
+                try:
+                    for j in range(sousseg):
+                        ov2, ovv = gmsh.model.occ.fuse([lines[i+8]], [tool[-1-j]])
+                        gmsh.model.occ.synchronize()
+
+                except:
+                    print("pas possible")
+                else:
+                    if len(ov2)>2:
+                        sousseg = sousseg+1
+                        tool = [ov2[-1],ov2[-2]]
+                        print(tool)
+
+    lines = gmsh.model.getEntities(1)
+    for i in range(len(lines)-8):
+        actual_surf = gmsh.model.getEntities(2)
+        try:
+            for ns in range(actual_surf):
+                if ns != (2,3):
+                    ov2, ovv = gmsh.model.occ.fragment([ns],[lines[i+8]])
+                    gmsh.model.occ.synchronize()
+        except:
+            print("pas possible")
+
     gmsh.model.occ.synchronize()
 
-    NN = int(10/density)
-    ite = 0
-    for c in range(8):
-        if c > 3:
-            NN=int(20/density)
+    #NN = int(10/density)
+    #ite = 0
+    #for c in range(8):
+        #if c > 3:
+        #    NN=int(20/density)
         #points = gmsh.model.getBoundary([c])
-        gmsh.model.mesh.setTransfiniteCurve(c+1, NN)
+        #gmsh.model.mesh.setTransfiniteCurve(c+1, NN)
     #for s in gmsh.model.getEntities(2):
     #    gmsh.model.mesh.setTransfiniteSurface(s[1])
 
@@ -142,11 +167,10 @@ def create_2d_rve_with_fibers(ep, density, num_fibers, fiber_length, ref_angle, 
 plt.ion()
 envelope_thickness = 20
 mesh_density = 0.5
-num_fibers = 100
+num_fibers = 20
 fiber_length = 50
 reference_angle = np.pi /4  # Example: 45 degrees as the reference angle
 angle_spread = np.pi/6  # Example: ±10 degrees spread
 position_spread = 50  # Example: Spread of 10 units from the center
 distribution_type = 'uniform'  # Options: 'uniform' or 'normal'
 create_2d_rve_with_fibers(envelope_thickness, mesh_density, num_fibers, fiber_length, reference_angle, angle_spread, position_spread, distribution_type)
-
